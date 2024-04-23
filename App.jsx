2a88fdf02768346b77d51aa1b4c599cc6a8bb2fb
hari11918 <49097224+hari11918@users.noreply.github.com>
2024-04-23 00:29:40 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css'; // Assuming you have a CSS file for styles
+
+import EmployeeManager from './EmployeeManager'; // Import EmployeeManager
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeManager />
+    </div>
+  );
+}
+
+export default App;
